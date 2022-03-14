8416f241248901d3a90af05df201b056668f8194
Jason Lambert <jmanhero247@gmail.com>
2022-03-14 16:37:01 -0700

    fixed ifdef

--- MaaX_v3_tensorflow_lite_micro_label_image_cm7/video/avt-ili9881c.h
+++ MaaX_v3_tensorflow_lite_micro_label_image_cm7/video/avt-ili9881c.h
@@ -8,6 +8,7 @@
 
 #ifndef _AVT_ILI9881C_H_
 #define _AVT_ILI9881C_H_
+#endif
 
 #include "fsl_display.h"
 #include "fsl_mipi_dsi_cmd.h"
@@ -87,5 +88,3 @@ status_t ILI9881C_Stop(display_handle_t *handle);
 #if defined(__cplusplus)
 }
 #endif
-
-#endif /* _FSL_RM68200_H_ */
