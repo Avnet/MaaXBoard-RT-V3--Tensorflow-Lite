0ede489d566be22a632a73dab62bb39698787898
MaximSaka <88205887+MaximSaka@users.noreply.github.com>
2022-12-02 15:46:15 -0800

    Add support for the AES-ACC-MAAX-DISP2

--- MaaX_v3_tensorflow_lite_micro_label_image_cm7/video/avt-ili9881c.h
+++ MaaX_v3_tensorflow_lite_micro_label_image_cm7/video/avt-ili9881c.h
@@ -8,7 +8,6 @@
 
 #ifndef _AVT_ILI9881C_H_
 #define _AVT_ILI9881C_H_
-#endif
 
 #include "fsl_display.h"
 #include "fsl_mipi_dsi_cmd.h"
@@ -16,46 +15,6 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-
-#define ILI9881C_DelayMs VIDEO_DelayMs
-#define ILI9881C VIDEO_DelayMs
-
-enum ili9881c_op {
-	ILI9881C_SWITCH_PAGE,
-	ILI9881C_COMMAND,
-};
-
-struct ili9881c_instr {
-	enum ili9881c_op	op;
-
-	union arg {
-		struct cmd {
-			uint8_t	cmd;
-			uint8_t	data;
-		} cmd;
-		uint8_t	page;
-	} arg;
-};
-
-#define ILI9881C_SWITCH_PAGE_INSTR(_page)	\
-	{					\
-		.op = ILI9881C_SWITCH_PAGE,	\
-		.arg = {			\
-			.page = (_page),	\
-		},				\
-	}
-
-#define ILI9881C_COMMAND_INSTR(_cmd, _data)	\
-	{					\
-		.op = ILI9881C_COMMAND,		\
-		.arg = {			\
-			.cmd = {		\
-				.cmd = (_cmd),	\
-				.data = (_data),\
-			},			\
-		},				\
-	}
-
 
 /*!
  * @brief ILI9881C resource.
@@ -88,3 +47,5 @@ status_t ILI9881C_Stop(display_handle_t *handle);
 #if defined(__cplusplus)
 }
 #endif
+
+#endif /* _FSL_RM68200_H_ */
